9a5fbb30e70d88e1894a9ad690da6b60d64842b5
Fish Chen <peterleo1202@gmail.com>
2019-12-03 18:11:47 +0800

    新增參考url

--- models/users/users.go
+++ models/users/users.go
@@ -1,3 +1,5 @@
 package users
 
 // TODO: add firebase authentication
+// https://firebase.google.com/docs/admin/setup?authuser=1
+// https://firebase.google.com/docs/auth/admin/manage-users?authuser=1
