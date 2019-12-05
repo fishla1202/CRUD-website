5a9657d8a5d39bf073b190f357e1f74667e74f16
Fish Chen <peterleo1202@gmail.com>
2019-12-05 16:51:27 +0800

    create user feature

--- controller/usersController.go
+++ controller/usersController.go
@@ -1,16 +1,32 @@
 package controller
 
 import (
+	"fmt"
+	"golang_side_project_crud_website/models/users"
 	"golang_side_project_crud_website/render_templates"
+	"log"
 	"net/http"
 	"path"
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	pageContent := PageContent{
-		PageTitle: "Sign up",
-		PageQuery: nil,
+	if r.Method == "GET" {
+		pageContent := PageContent{
+			PageTitle: "Sign up",
+			PageQuery: nil,
+		}
+		index := path.Join("templates/users", "create.html")
+		render_templates.ReturnRenderTemplate(w, index, &pageContent)
+	}else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil { log.Fatal(err)}
+
+		uid := users.CreateUser(
+			r.Form["userName"][0], r.Form["userEmail"][0], r.Form["userPwd"][0])
+		fmt.Println(uid)
+		// TODO: get the uid and store in session when user create post add the uid to it
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}else {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
-	index := path.Join("templates/users", "create.html")
-	render_templates.ReturnRenderTemplate(w, index, &pageContent)
 }
--- models/users/users.go
+++ models/users/users.go
@@ -7,6 +7,9 @@ package users
 
 import (
 	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
+	"path"
+
 	//"firebase.google.com/go/auth"
 	"fmt"
 	"golang.org/x/net/context"
@@ -15,12 +18,31 @@ import (
 	"os"
 )
 
-func mian() {
-	opt := option.WithCredentialsFile(os.Getenv("FIREBASE_KEY_PATH"))
-	app, err := firebase.NewApp(context.Background(), nil, opt)
+func CreateUser(userName string, userEmail string, userPwd string) string{
+	opt := option.WithCredentialsFile(path.Join(os.Getenv("FIREBASE_KEY_PATH")))
+	ctx := context.Background()
+	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
 		log.Fatal("error initializing app:", err)}
 	fmt.Println(app)
+
+	client, err := app.Auth(ctx)
+	if err != nil {
+		log.Fatalf("error creating user: %v\n", err)
+	}
+
+	params := (&auth.UserToCreate{}).
+		Email(userEmail).
+		EmailVerified(false).
+		Password(userPwd).
+		DisplayName(userName).
+		Disabled(false)
+	u, err := client.CreateUser(ctx, params)
+	if err != nil {
+		log.Fatalf("error creating user: %v\n", err)
+	}
+	log.Printf("Successfully created user: %v\n", u)
+	return u.UID
 }
 
 
