63e0074cf94b70853dcc80e0c0aa6dc3ccf1a9df
Fish Chen <peterleo1202@gmail.com>
2019-12-09 15:05:31 +0800

    add TODO session

--- controller/homeController.go
+++ controller/homeController.go
@@ -4,6 +4,8 @@ import (
 	"github.com/jinzhu/gorm"
 	"golang_side_project_crud_website/models/posts"
 	"golang_side_project_crud_website/render_templates"
+	//"github.com/gorilla/sessions"
+	"log"
 	"net/http"
 	"path"
 )
@@ -13,16 +15,22 @@ type PageContent struct {
 	PageQuery *gorm.DB
 }
 
+// TODO: 將login firebase 拿到的uid 存在session裡
 func IndexHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		allPosts := posts.FindAllPosts()
 
-	allPosts := posts.FindAllPosts()
+		pageContent := PageContent{
+			PageTitle: "FishLa",
+			PageQuery: allPosts,
+		}
 
-	pageContent := PageContent{
-		PageTitle: "FishLa",
-		PageQuery: allPosts,
+		index := path.Join("templates", "index.html")
+		render_templates.ReturnRenderTemplate(w, index, &pageContent)
+	}else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {log.Fatal(err)}
+		log.Fatal(r.Form)
 	}
-
-	index := path.Join("templates", "index.html")
-	render_templates.ReturnRenderTemplate(w, index, &pageContent)
 
 }
