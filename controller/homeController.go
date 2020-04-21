94711a0bfce681ff9cc0f603405f5358dfa1c0e2
Fish <fishchen@mytaiwantour.com>
2020-04-21 18:43:48 +0800

    PageContent constructor function for display collections in all page

--- controller/homeController.go
+++ controller/homeController.go
@@ -14,7 +14,20 @@ type PageContent struct {
 	PageQuery interface{}
 	CsrfTag template.HTML
 	IsUser bool
+	Collections [] models.Collection
 }
+
+func NewPageContent () *PageContent {
+	var pageContent PageContent
+	collections, err := models.FindAllCollections()
+	if err != nil {
+
+	}
+	pageContent.Collections = collections
+
+	return &pageContent
+}
+
 
 func IndexHandle(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
@@ -26,12 +39,15 @@ func IndexHandle(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	pageContent := PageContent{
-		PageTitle: "Discuss",
-		PageQuery: allPosts,
-		IsUser: isUser,
-	}
+	pageContent := NewPageContent()
+	pageContent.PageTitle = "Discuss"
+	pageContent.PageQuery = allPosts
+	pageContent.IsUser = isUser
+	//pageContent := PageContent{
+	//	PageTitle: "Discuss",
+	//	PageQuery: allPosts,
+	//	IsUser: isUser,
+	//}
 
 	index := path.Join("templates", "index.html")
 	render_templates.ReturnRenderTemplate(w, index, &pageContent)
