95112d484f59f20198642ab09f4dfdd289c0a2fd
Fish <fishchen@mytaiwantour.com>
2020-04-22 10:35:12 +0800

    將 controller共用的方法移到 統一檔案管理

---
+++ controller/controller.go
@@ -0,0 +1,25 @@
+package controller
+
+import (
+	"golang_side_project_CRUD_website/models"
+	"html/template"
+)
+
+type PageContent struct {
+	PageTitle string
+	PageQuery interface{}
+	CsrfTag template.HTML
+	IsUser bool
+	Collections [] models.Collection
+}
+
+func newPageContent () *PageContent {
+	var pageContent PageContent
+	collections, err := models.FindAllCollections()
+	if err != nil {
+
+	}
+	pageContent.Collections = collections
+
+	return &pageContent
+}
--- controller/homeController.go
+++ controller/homeController.go
@@ -4,29 +4,9 @@ import (
 	"golang_side_project_crud_website/config"
 	"golang_side_project_crud_website/models"
 	"golang_side_project_crud_website/render_templates"
-	"html/template"
 	"net/http"
 	"path"
 )
-
-type PageContent struct {
-	PageTitle string
-	PageQuery interface{}
-	CsrfTag template.HTML
-	IsUser bool
-	Collections [] models.Collection
-}
-
-func NewPageContent () *PageContent {
-	var pageContent PageContent
-	collections, err := models.FindAllCollections()
-	if err != nil {
-
-	}
-	pageContent.Collections = collections
-
-	return &pageContent
-}
 
 
 func IndexHandle(w http.ResponseWriter, r *http.Request) {
@@ -39,15 +19,10 @@ func IndexHandle(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	pageContent := NewPageContent()
+	pageContent := newPageContent()
 	pageContent.PageTitle = "Discuss"
 	pageContent.PageQuery = allPosts
 	pageContent.IsUser = isUser
-	//pageContent := PageContent{
-	//	PageTitle: "Discuss",
-	//	PageQuery: allPosts,
-	//	IsUser: isUser,
-	//}
 
 	index := path.Join("templates", "index.html")
 	render_templates.ReturnRenderTemplate(w, index, &pageContent)
